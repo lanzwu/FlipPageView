a840427153922fdf81900d852865b8a02c9d290e
lanzwu <lanzwuzxy@gmail.com>
2018-04-02 00:01:15 +0800

    Calculate the basic points.

--- app/src/main/java/com/lanzwuzxy/flippageview/FlipPageView.java
+++ app/src/main/java/com/lanzwuzxy/flippageview/FlipPageView.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -17,7 +18,7 @@ public class FlipPageView extends View {
     private Canvas canvas;
     private Bitmap page;
     private PointF finger, start_l, end_l, control_l, start_r, end_r, control_r;
-    private PointF corner, pointG, pointE;
+    private PointF corner, pointG, pointE, pointH, pointC, pointJ, pointB, pointK, pointD, pointI;
     private int screenWidth, screenHeight;
 
     FlipPageView(Context context) {
@@ -29,18 +30,49 @@ public class FlipPageView extends View {
         screenWidth = dm.widthPixels;
         screenHeight = dm.heightPixels;
 
+
         mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
         mPaint.setStrokeWidth(3);
+        mPaint.setTextSize(20);
         mPaint.setAntiAlias(true);
 
         page = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(page);
+        init();
+    }
+
+    private void init() {
+        finger = new PointF(0, 0);
+        corner = new PointF(0, 0);
+        pointG = new PointF(0, 0);
+        pointE = new PointF(0, 0);
+        pointH = new PointF(0, 0);
+        pointC = new PointF(0, 0);
+        pointJ = new PointF(0, 0);
+        pointB = new PointF(0, 0);
+        pointK = new PointF(0, 0);
+        pointD = new PointF(0, 0);
+        pointI = new PointF(0, 0);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawCircle(finger.x, finger.y, 10, mPaint);
+        canvas.drawCircle(corner.x, corner.y, 10, mPaint);
+        canvas.drawText("E", pointE.x, pointE.y, mPaint);
+        canvas.drawText("G", pointG.x, pointG.y, mPaint);
+        canvas.drawText("H", pointH.x, pointH.y, mPaint);
+
+        canvas.drawText("J", pointJ.x, pointJ.y, mPaint);
+        canvas.drawText("C", pointC.x, pointC.y, mPaint);
+
+        canvas.drawText("B", pointB.x, pointB.y, mPaint);
+        canvas.drawText("K", pointK.x, pointK.y, mPaint);
+
+        canvas.drawText("D", pointD.x, pointD.y, mPaint);
+        canvas.drawText("I", pointI.x, pointI.y, mPaint);
     }
 
 
@@ -48,20 +80,41 @@ public class FlipPageView extends View {
     public boolean onTouchEvent(MotionEvent event) {
 
         finger = new PointF(event.getX(), event.getY());
-        if (finger.x > (screenWidth - 100) && finger.x < screenWidth
-                && finger.y > (screenHeight - 100) && finger.y < screenHeight) {
+        if (finger.x > screenWidth / 2 && finger.x < screenWidth
+                && finger.y > screenHeight / 2 && finger.y < screenHeight) {
             corner = new PointF(screenWidth, screenHeight);
-        } else if (finger.x > (screenWidth - 100) && finger.x < screenWidth
-                && finger.y > 0 && finger.y < 100) {
+        } else if (finger.x > screenWidth / 2 && finger.x < screenWidth
+                && finger.y > 0 && finger.y < screenHeight / 2) {
             corner = new PointF(screenWidth, 0);
         }
         pointG = new PointF((finger.x + corner.x) / 2, (finger.y + corner.y) / 2);
-        pointE = new PointF(pointG.x - (screenWidth - pointG.y) *
-                (screenWidth - pointG.y) / (screenWidth - pointG.x), screenHeight);
+
+        pointE = new PointF(pointG.x -
+                (corner.y - pointG.y) * (corner.y - pointG.y) / (corner.x - pointG.x), corner.y);
+        pointH = new PointF(corner.x, pointG.y -
+                (corner.x - pointG.x) * (corner.x - pointG.x) / (corner.y - pointG.y));
+
+        pointJ = new PointF(corner.x, pointH.y - (corner.y - pointH.y) / 2);
+        pointC = new PointF(pointE.x - (corner.x - pointE.x) / 2, corner.y);
+
+        pointB = cross(finger, pointE, pointC, pointJ);
+        pointK = cross(finger, pointH, pointC, pointJ);
+
+        pointD = new PointF((pointC.x + 2 * pointE.x + pointB.x) / 4, (pointC.y + 2 * pointE.y + pointB.y) / 4);
+        pointI = new PointF((pointJ.x + 2 * pointH.x + pointK.x) / 4, (pointJ.y + 2 * pointH.y + pointK.y) / 4);
 
 
+        postInvalidate();
         performClick();
-        return super.onTouchEvent(event);
+        return true;
+    }
+
+    private PointF cross(PointF p1, PointF p2, PointF p3, PointF p4) {
+        float crossX = ((p1.x - p2.x) * (p3.x * p4.y - p4.x * p3.y) - (p3.x - p4.x) * (p1.x * p2.y - p2.x * p1.y))
+                / ((p3.x - p4.x) * (p1.y - p2.y) - (p1.x - p2.x) * (p3.y - p4.y));
+        float crossY = ((p1.y - p2.y) * (p3.x * p4.y - p4.x * p3.y) - (p1.x * p2.y - p2.x * p1.y) * (p3.y - p4.y))
+                / ((p1.y - p2.y) * (p3.x - p4.x) - (p1.x - p2.x) * (p3.y - p4.y));
+        return new PointF(crossX, crossY);
     }
 
     @Override
--- app/src/main/java/com/lanzwuzxy/flippageview/MainActivity.java
+++ app/src/main/java/com/lanzwuzxy/flippageview/MainActivity.java
@@ -1,13 +1,16 @@
 package com.lanzwuzxy.flippageview;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(new FlipPageView(this));
     }
 }
