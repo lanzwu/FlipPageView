6f4518ed6b4cdcfba905052b68da7bd8698a0aeb
lanzwu <lanzwuzxy@gmail.com>
2018-04-02 20:35:25 +0800

    draw view path.

--- app/src/main/java/com/lanzwuzxy/flippageview/FlipPageView.java
+++ app/src/main/java/com/lanzwuzxy/flippageview/FlipPageView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -17,6 +18,7 @@ public class FlipPageView extends View {
     private Paint mPaint;
     private Canvas canvas;
     private Bitmap page;
+    private Path cornerPath;
     private PointF finger, start_l, end_l, control_l, start_r, end_r, control_r;
     private PointF corner, pointG, pointE, pointH, pointC, pointJ, pointB, pointK, pointD, pointI;
     private int screenWidth, screenHeight;
@@ -34,11 +36,14 @@ public class FlipPageView extends View {
         mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
         mPaint.setStrokeWidth(3);
+        mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setTextSize(20);
         mPaint.setAntiAlias(true);
 
         page = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(page);
+
+        cornerPath = new Path();
         init();
     }
 
@@ -61,18 +66,22 @@ public class FlipPageView extends View {
         super.onDraw(canvas);
         canvas.drawCircle(finger.x, finger.y, 10, mPaint);
         canvas.drawCircle(corner.x, corner.y, 10, mPaint);
-        canvas.drawText("E", pointE.x, pointE.y, mPaint);
-        canvas.drawText("G", pointG.x, pointG.y, mPaint);
-        canvas.drawText("H", pointH.x, pointH.y, mPaint);
+//        canvas.drawText("E", pointE.x, pointE.y, mPaint);
+//        canvas.drawText("G", pointG.x, pointG.y, mPaint);
+//        canvas.drawText("H", pointH.x - 10, pointH.y, mPaint);
+//
+//        canvas.drawText("J", pointJ.x - 10, pointJ.y, mPaint);
+//        canvas.drawText("C", pointC.x, pointC.y, mPaint);
+//
+//        canvas.drawText("B", pointB.x, pointB.y, mPaint);
+//        canvas.drawText("K", pointK.x, pointK.y, mPaint);
+//
+//        canvas.drawText("D", pointD.x, pointD.y, mPaint);
+//        canvas.drawText("I", pointI.x, pointI.y, mPaint);
 
-        canvas.drawText("J", pointJ.x, pointJ.y, mPaint);
-        canvas.drawText("C", pointC.x, pointC.y, mPaint);
 
-        canvas.drawText("B", pointB.x, pointB.y, mPaint);
-        canvas.drawText("K", pointK.x, pointK.y, mPaint);
 
-        canvas.drawText("D", pointD.x, pointD.y, mPaint);
-        canvas.drawText("I", pointI.x, pointI.y, mPaint);
+        canvas.drawPath(cornerPath, mPaint);
     }
 
 
@@ -87,26 +96,40 @@ public class FlipPageView extends View {
                 && finger.y > 0 && finger.y < screenHeight / 2) {
             corner = new PointF(screenWidth, 0);
         }
-        pointG = new PointF((finger.x + corner.x) / 2, (finger.y + corner.y) / 2);
+        pointG = new PointF((finger.x + corner.x) / 2f, (finger.y + corner.y) / 2f);
 
         pointE = new PointF(pointG.x -
                 (corner.y - pointG.y) * (corner.y - pointG.y) / (corner.x - pointG.x), corner.y);
         pointH = new PointF(corner.x, pointG.y -
                 (corner.x - pointG.x) * (corner.x - pointG.x) / (corner.y - pointG.y));
 
-        pointJ = new PointF(corner.x, pointH.y - (corner.y - pointH.y) / 2);
-        pointC = new PointF(pointE.x - (corner.x - pointE.x) / 2, corner.y);
+        pointJ = new PointF(corner.x, pointH.y - (corner.y - pointH.y) / 2f);
+        pointC = new PointF(pointE.x - (corner.x - pointE.x) / 2f, corner.y);
 
-        pointB = cross(finger, pointE, pointC, pointJ);
-        pointK = cross(finger, pointH, pointC, pointJ);
+//        pointB = cross(finger, pointE, pointC, pointJ);
+//        pointK = cross(finger, pointH, pointC, pointJ);
+        pointB = new PointF((finger.x + pointE.x) / 2, (finger.y + pointE.y) / 2);
+        pointK = new PointF((finger.x + pointH.x) / 2, (finger.y + pointH.y) / 2);
 
-        pointD = new PointF((pointC.x + 2 * pointE.x + pointB.x) / 4, (pointC.y + 2 * pointE.y + pointB.y) / 4);
-        pointI = new PointF((pointJ.x + 2 * pointH.x + pointK.x) / 4, (pointJ.y + 2 * pointH.y + pointK.y) / 4);
+        pointD = new PointF((pointC.x + 2f * pointE.x + pointB.x) / 4f, (pointC.y + 2f * pointE.y + pointB.y) / 4f);
+        pointI = new PointF((pointJ.x + 2f * pointH.x + pointK.x) / 4f, (pointJ.y + 2f * pointH.y + pointK.y) / 4f);
 
+        drawPath();
 
-        postInvalidate();
         performClick();
         return true;
+    }
+
+    private void drawPath(){
+        cornerPath.reset();
+        cornerPath.moveTo(corner.x,corner.y);
+        cornerPath.lineTo(pointC.x, pointC.y);
+        cornerPath.quadTo(pointE.x, pointE.y, pointB.x, pointB.y);
+        cornerPath.lineTo(finger.x, finger.y);
+        cornerPath.lineTo(pointK.x, pointK.y);
+        cornerPath.quadTo(pointH.x, pointH.y, pointJ.x, pointJ.y);
+        cornerPath.close();
+        postInvalidate();
     }
 
     private PointF cross(PointF p1, PointF p2, PointF p3, PointF p4) {
