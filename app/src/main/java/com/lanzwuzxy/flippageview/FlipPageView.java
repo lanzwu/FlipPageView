9612631f858d18e9cf61f8520e9cf84c33b961e4
lanzwu <lanzwuzxy@gmail.com>
2018-04-20 16:28:12 +0800

    (1) finish draw path (2) add determination of the touch point , and recalculate "finger" point when real finger point out of given range.

--- app/src/main/java/com/lanzwuzxy/flippageview/FlipPageView.java
+++ app/src/main/java/com/lanzwuzxy/flippageview/FlipPageView.java
@@ -19,8 +19,7 @@ public class FlipPageView extends View {
     private Canvas canvas;
     private Bitmap page;
     private Path cornerPath;
-    private PointF finger, start_l, end_l, control_l, start_r, end_r, control_r;
-    private PointF corner, pointG, pointE, pointH, pointC, pointJ, pointB, pointK, pointD, pointI;
+    private PointF finger, corner, pointG, pointE, pointH, pointC, pointJ, pointB, pointK, pointD, pointI;
     private int screenWidth, screenHeight;
 
     FlipPageView(Context context) {
@@ -28,14 +27,16 @@ public class FlipPageView extends View {
 
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics dm = new DisplayMetrics();
-        wm.getDefaultDisplay().getMetrics(dm);
+        if (wm != null) {
+            wm.getDefaultDisplay().getMetrics(dm);
+        }
         screenWidth = dm.widthPixels;
         screenHeight = dm.heightPixels;
 
 
         mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
-        mPaint.setStrokeWidth(3);
+        mPaint.setStrokeWidth(1);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setTextSize(20);
         mPaint.setAntiAlias(true);
@@ -49,7 +50,7 @@ public class FlipPageView extends View {
 
     private void init() {
         finger = new PointF(0, 0);
-        corner = new PointF(0, 0);
+        corner = new PointF(screenWidth, screenHeight);
         pointG = new PointF(0, 0);
         pointE = new PointF(0, 0);
         pointH = new PointF(0, 0);
@@ -64,8 +65,7 @@ public class FlipPageView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(finger.x, finger.y, 10, mPaint);
-        canvas.drawCircle(corner.x, corner.y, 10, mPaint);
+
 //        canvas.drawText("E", pointE.x, pointE.y, mPaint);
 //        canvas.drawText("G", pointG.x, pointG.y, mPaint);
 //        canvas.drawText("H", pointH.x - 10, pointH.y, mPaint);
@@ -79,56 +79,90 @@ public class FlipPageView extends View {
 //        canvas.drawText("D", pointD.x, pointD.y, mPaint);
 //        canvas.drawText("I", pointI.x, pointI.y, mPaint);
 
-
-
         canvas.drawPath(cornerPath, mPaint);
     }
 
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        finger.set(event.getX(), event.getY());
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (finger.x > screenWidth / 2 && finger.x < screenWidth
+                        && finger.y > screenHeight / 4 * 3 && finger.y < screenHeight) {
+                    corner.set(screenWidth, screenHeight);
+                } else if (finger.x > screenWidth / 2 && finger.x < screenWidth
+                        && finger.y > 0 && finger.y < screenHeight / 4) {
+                    corner.set(screenWidth, 0);
+                }
 
-        finger = new PointF(event.getX(), event.getY());
-        if (finger.x > screenWidth / 2 && finger.x < screenWidth
-                && finger.y > screenHeight / 2 && finger.y < screenHeight) {
-            corner = new PointF(screenWidth, screenHeight);
-        } else if (finger.x > screenWidth / 2 && finger.x < screenWidth
-                && finger.y > 0 && finger.y < screenHeight / 2) {
-            corner = new PointF(screenWidth, 0);
+                if (finger.x > screenWidth / 5 * 2 && finger.x < screenWidth
+                        && finger.y > screenHeight / 4 && finger.y < screenHeight / 4 * 3) {
+                    finger.set(event.getX(), event.getY() - 10);
+                }
+            case MotionEvent.ACTION_MOVE:
+
+                calculatePoint(finger.x, finger.y);
+
+                if (pointC.x <= 0) {
+                    reCalculateFinger();
+                }
+                drawPath(false);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawPath(true);
+                break;
         }
-        pointG = new PointF((finger.x + corner.x) / 2f, (finger.y + corner.y) / 2f);
-
-        pointE = new PointF(pointG.x -
-                (corner.y - pointG.y) * (corner.y - pointG.y) / (corner.x - pointG.x), corner.y);
-        pointH = new PointF(corner.x, pointG.y -
-                (corner.x - pointG.x) * (corner.x - pointG.x) / (corner.y - pointG.y));
-
-        pointJ = new PointF(corner.x, pointH.y - (corner.y - pointH.y) / 2f);
-        pointC = new PointF(pointE.x - (corner.x - pointE.x) / 2f, corner.y);
-
-//        pointB = cross(finger, pointE, pointC, pointJ);
-//        pointK = cross(finger, pointH, pointC, pointJ);
-        pointB = new PointF((finger.x + pointE.x) / 2, (finger.y + pointE.y) / 2);
-        pointK = new PointF((finger.x + pointH.x) / 2, (finger.y + pointH.y) / 2);
-
-        pointD = new PointF((pointC.x + 2f * pointE.x + pointB.x) / 4f, (pointC.y + 2f * pointE.y + pointB.y) / 4f);
-        pointI = new PointF((pointJ.x + 2f * pointH.x + pointK.x) / 4f, (pointJ.y + 2f * pointH.y + pointK.y) / 4f);
-
-        drawPath();
 
         performClick();
         return true;
     }
 
-    private void drawPath(){
+    private void calculatePoint(float x, float y) {
+        pointG.set((x + corner.x) / 2f, (y + corner.y) / 2f);
+
+        pointE.set(pointG.x -
+                (corner.y - pointG.y) * (corner.y - pointG.y) / (corner.x - pointG.x), corner.y);
+        pointH.set(corner.x, pointG.y -
+                (corner.x - pointG.x) * (corner.x - pointG.x) / (corner.y - pointG.y));
+
+        pointC.set(pointE.x - (corner.x - pointE.x) / 2f, corner.y);
+        pointJ.set(corner.x, pointH.y - (corner.y - pointH.y) / 2f);
+
+        pointB.set((x + pointE.x) / 2, (y + pointE.y) / 2);
+        pointK.set((x + pointH.x) / 2, (y + pointH.y) / 2);
+
+        pointD.set((pointC.x + 2f * pointE.x + pointB.x) / 4f, (pointC.y + 2f * pointE.y + pointB.y) / 4f);
+        pointI.set((pointJ.x + 2f * pointH.x + pointK.x) / 4f, (pointJ.y + 2f * pointH.y + pointK.y) / 4f);
+    }
+
+    private void reCalculateFinger() {
+        PointF fingerCal = new PointF();
+        float w0 = screenWidth - pointC.x;
+
+        float w1 = Math.abs(corner.x - finger.x);
+        float w2 = screenWidth * w1 / w0;
+        fingerCal.x = Math.abs(corner.x - w2);
+
+        float h1 = Math.abs(corner.y - finger.y);
+        float h2 = w2 * h1 / w1;
+        fingerCal.y = Math.abs(corner.y - h2);
+        finger.set(fingerCal.x, fingerCal.y);
+
+        calculatePoint(finger.x, finger.y);
+    }
+
+    private void drawPath(boolean clean) {
         cornerPath.reset();
-        cornerPath.moveTo(corner.x,corner.y);
-        cornerPath.lineTo(pointC.x, pointC.y);
-        cornerPath.quadTo(pointE.x, pointE.y, pointB.x, pointB.y);
-        cornerPath.lineTo(finger.x, finger.y);
-        cornerPath.lineTo(pointK.x, pointK.y);
-        cornerPath.quadTo(pointH.x, pointH.y, pointJ.x, pointJ.y);
-        cornerPath.close();
+        if (!clean) {
+            cornerPath.moveTo(corner.x, corner.y);
+            cornerPath.lineTo(pointC.x, pointC.y);
+            cornerPath.quadTo(pointE.x, pointE.y, pointB.x, pointB.y);
+            cornerPath.lineTo(finger.x, finger.y);
+            cornerPath.lineTo(pointK.x, pointK.y);
+            cornerPath.quadTo(pointH.x, pointH.y, pointJ.x, pointJ.y);
+            cornerPath.close();
+        }
         postInvalidate();
     }
 
